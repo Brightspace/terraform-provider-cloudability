8ed256e4bf202a586382c6a2d941965d5c3c5b6f
jrbeverly <Jonathan.Beverly@D2L.com>
2019-11-07 16:59:11 -0500

    Outputs for role_name and external_id

--- examples/data-account/main.tf
+++ examples/data-account/main.tf
@@ -3,4 +3,7 @@ data "cloudability_account" "cloudability" {
 }
 
 variable "account_id" { type = string }
+
 output "id" { value = data.cloudability_account.cloudability.id }
+output "role_name" { value = data.cloudability_account.cloudability.role_name }
+output "external_id" { value = data.cloudability_account.cloudability.external_id }
